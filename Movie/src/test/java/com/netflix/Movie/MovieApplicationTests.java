33d55c2d3d9263af489eb338d5ecc1c652167fe0
nagendraalavala <nagendra.alavala@gmail.com>
2020-06-27 19:20:59 -0500

    fresh code

---
+++ Movie/src/test/java/com/netflix/Movie/MovieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.netflix.Movie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
